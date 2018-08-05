62d9afb654e99ee9222813e4c049f397b50002a5
Nat Knight <nathaniel.ep@gmail.com>
2018-08-05 08:58:35 -0700

    Add default methods for color and screen
    Also change trait name from DrawInto to Drawable.

--- libsmashbing/src/draw.rs
+++ libsmashbing/src/draw.rs
@@ -3,10 +3,18 @@
 /// RGBA Color
 pub type Color = (f32, f32, f32, f32);
 
+fn blank_color() -> Color {
+    (0.0, 0.0, 0.0, 0.0)
+}
+
 /// Field to draw pixels on
 pub type Screen = [[Color; 64]; 64];
 
-trait DrawInto {
+pub fn blank_screen() -> Screen {
+    [[(0.0, 0.0, 0.0, 0.0); 64]; 64]
+}
+
+pub trait Drawable {
     /// Draw this object into the given screen.
     fn draw_into(&self, scrn: &mut Screen);
 }
