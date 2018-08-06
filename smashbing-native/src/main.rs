e0936ad654e9ceeb43ddd57ae290582f128ca67a
Nat Knight <nathaniel.ep@gmail.com>
2018-08-06 11:03:00 -0700

    Handle pointer input
    This also required a re-work of the graphics:
    
     - Setting resolution to 64x64 didn't seem to do anything
     - I'm converting between pixels and game coords myself
     - The internal interface for handling input changed slightly

--- smashbing-native/src/main.rs
+++ smashbing-native/src/main.rs
@@ -4,15 +4,14 @@ extern crate libsmashbing;
 use ggez::conf;
 use ggez::event;
 use ggez::graphics;
+use ggez::mouse;
 use ggez::timer;
 use ggez::{Context, GameResult};
 
 use libsmashbing::draw;
-use libsmashbing::rect;
 use libsmashbing::Game;
 
 fn setup_graphics(ctx: &mut Context) -> GameResult<()> {
-    graphics::set_resolution(ctx, 64, 64)?;
     graphics::set_fullscreen(ctx, true)?;
     graphics::set_screen_coordinates(ctx, graphics::Rect::new(0.0, 0.0, 64.0, 64.0))?;
     graphics::set_background_color(ctx, graphics::BLACK);
@@ -24,26 +23,49 @@ fn convert_color(inp: &draw::Color) -> graphics::Color {
     graphics::Color::new(r, g, b, a)
 }
 
-fn convert_rect(inp: &rect::Rect) -> graphics::Rect {
-    graphics::Rect::new(
-        inp.left,
-        64.0 - inp.top,
-        (inp.right - inp.left),
-        (inp.top - inp.bottom),
-    )
+struct CoordConverter {
+    screen_size: (f32, f32),
+}
+
+impl CoordConverter {
+    fn convert_from_pixels(&self, px: f32, py: f32) -> (f32, f32) {
+        const DOMAIN_SIZE: f32 = 64.0;
+        let (sx, sy) = self.screen_size;
+        let x = px / sx * DOMAIN_SIZE;
+        let vy = py / sy * DOMAIN_SIZE;
+        let y = 64.0 - vy;
+        (x, y)
+    }
+
+    fn new(ctx: &mut Context) -> CoordConverter {
+        let (sx, sy) = graphics::get_size(ctx);
+        CoordConverter {
+            screen_size: (sx as f32, sy as f32),
+        }
+    }
 }
 
 struct NativeGame {
     game: Game,
+    fire: bool,
+    coords: CoordConverter,
 }
 
 impl event::EventHandler for NativeGame {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         const TARGET_FPS: u32 = 60;
+        let cmds = if self.fire {
+            self.fire = false; // Reset listener.
+            let pos = mouse::get_position(ctx).expect("Error getting mouse position");
+            let (x, y) = self.coords.convert_from_pixels(pos.x, pos.y);
+            println!("firing at x: {}, y: {}", x, y);
+            vec![libsmashbing::Command::Fire(x, y)]
+        } else {
+            Vec::new()
+        };
         while timer::check_update_time(ctx, TARGET_FPS) {
             let delta = timer::get_delta(ctx);
             let dt = timer::duration_to_f64(delta);
-            let cmds = [];
             self.game.update(dt as f32, &cmds);
         }
         Ok(())
@@ -51,6 +73,7 @@ impl event::EventHandler for NativeGame {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
+
         // Ball
         graphics::set_color(ctx, convert_color(&draw::BALL_COLOR))?;
         let ball_rect =
@@ -58,9 +81,14 @@ impl event::EventHandler for NativeGame {
         graphics::rectangle(ctx, graphics::DrawMode::Fill, ball_rect)?;
 
         graphics::present(ctx);
-        println!("fps: {}", timer::get_fps(ctx));
         timer::yield_now();
         Ok(())
+    }
+
+    fn mouse_button_down_event(&mut self, _: &mut Context, _: event::MouseButton, _: i32, _: i32) {
+        if !self.fire {
+            self.fire = true;
+        }
     }
 }
 
@@ -69,8 +97,11 @@ fn main() {
     let ctx = &mut Context::load_from_conf("Ballistic Smashbing", "Nathaniel Knight", c)
         .expect("Error creating context");
     setup_graphics(ctx).expect("Error setting up graphics");
+    let converter = CoordConverter::new(ctx);
     let mut game = NativeGame {
         game: libsmashbing::Game::default(),
+        fire: false,
+        coords: converter,
     };
     event::run(ctx, &mut game).expect("Error running game");
 }
