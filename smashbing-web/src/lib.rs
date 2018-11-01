9362c9cae0409718e13a9ce23b806ec1c46335c4
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-11-01 15:30:48 -0700

    Add smashbing-web build and scaffold files
    Note that this only builds with an out-of-source copy of libsmashbing
    where the `rand` crate is excluded. `rand` has some WASM support, but
    libsmashbing will need some tweaking to take advantage of it.

--- smashbing-web/src/lib.rs
+++ smashbing-web/src/lib.rs
@@ -1,12 +1,33 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
+extern crate libsmashbing;
+
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub struct Game {
+    game: libsmashbing::Game,
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        Game {
+            game: libsmashbing::Game::default(),
+        }
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+impl Game {
+    #[wasm_bindgen]
+    pub fn get_ball_pos(&self) -> String {
+        let ball = &self.game.ball;
+        let x: f32 = ball.pos.x;
+        let y: f32 = ball.pos.y;
+        format!("Ball & ({}, {})", x, y)
+    }
+}
+
+#[wasm_bindgen]
+pub fn new_game() -> Game {
+    Game::default()
 }
