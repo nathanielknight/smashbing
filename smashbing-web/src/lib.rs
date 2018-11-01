ed719b0a1b4dc9e02c3d8c0edb2af20b78325042
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-11-01 16:14:02 -0700

    Create a game in the browser
    It's not interactive or rendered or anything yet, but it nominally runs.

--- smashbing-web/src/lib.rs
+++ smashbing-web/src/lib.rs
@@ -1,33 +1,9 @@
+extern crate libsmashbing;
 extern crate wasm_bindgen;
+
 use wasm_bindgen::prelude::*;
 
-extern crate libsmashbing;
-
 #[wasm_bindgen]
-pub struct Game {
-    game: libsmashbing::Game,
-}
-
-impl Default for Game {
-    fn default() -> Self {
-        Game {
-            game: libsmashbing::Game::default(),
-        }
-    }
-}
-
-#[wasm_bindgen]
-impl Game {
-    #[wasm_bindgen]
-    pub fn get_ball_pos(&self) -> String {
-        let ball = &self.game.ball;
-        let x: f32 = ball.pos.x;
-        let y: f32 = ball.pos.y;
-        format!("Ball & ({}, {})", x, y)
-    }
-}
-
-#[wasm_bindgen]
-pub fn new_game() -> Game {
-    Game::default()
+pub fn embed() {
+    let mut game = libsmashbing::Game::default();
 }
