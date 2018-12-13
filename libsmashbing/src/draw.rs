9aa540b789cc9bc7ab8fb387776784e08956bd10
Nat Knight <nathaniel.ep@gmail.com>
2018-12-12 22:11:18 -0800

    Add comments to block and draw

--- libsmashbing/src/draw.rs
+++ libsmashbing/src/draw.rs
@@ -1,4 +1,5 @@
-/// RGBA Color
+/// RGBA Color. This type is mostly here to provide a common way to send colors
+/// to implementors (e.g. SDL based native libraries, WASM based webpages).
 pub type Color = (f32, f32, f32, f32);
 
 pub const BALL_COLOR: Color = (1.0, 0.0, 0.0, 1.0);
